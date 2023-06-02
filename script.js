4f616d00c55b4e9de175d1566150ba563e1440ab
Alex C <alex.cipllone10@gmail.com>
2023-06-02 11:41:18 -0400

    added node.js to path on my machine

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Take it from promise chaining
 let stocks = 
     {
         Fruits: ["strawberry","grapes","banana","apples"],
@@ -7,34 +6,72 @@ let stocks =
         toppings: ["chocolate","peanuts"]
     };
 
-let order = (fruit_name, call_production) =>{
-    setTimeout(function(){
-        // console.log(`${stocks.Fruits[fruit_name]} was selected`);
 
-        call_production();
-    },2000);
-};
+let is_shop_open = true;
 
-let production = () =>{
-    setTimeout(()=>{
-        console.log("Production has started");
-    },0000);
-    setTimeout(()=>{
-        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} Added`)
-        setTimeout(()=>{
-          console.log("start the machine")
-          setTimeout(()=>{
-            console.log(`Ice cream placed on ${stocks.holder[1]}`)
+let order = (time,work) =>{
+
+    return new Promise ((resolve, reject)=>{
+        if(is_shop_open){
             setTimeout(()=>{
-              console.log(`${stocks.toppings[0]} as toppings`)
-              setTimeout(()=>{
-                console.log("serve Ice cream")
-              },2000)
-            },3000)
-          },2000)
-        },1000)
-      },1000)
-    setTimeout(()=>{
-        console.log("The fruit has been chopped");
-    },2000);
-};
+                // work is getting done here
+                resolve(work());
+
+                // Setting time here for 1 work
+            }, time)
+            
+        }
+        else{
+            reject(console.log("Our shop is closed"))
+        }
+    })
+
+}
+
+order(2000,()=>console.log(`${stocks.Fruits[0]} was selected`))
+
+// step 1
+order(2000,()=>console.log(`${stocks.Fruits[0]} was selected`))
+
+// step 2
+.then(()=>{
+  return order(0000,()=>console.log('production has started'))
+})
+
+// step 3
+.then(()=>{
+  return order(2000, ()=>console.log("Fruit has been chopped"))
+})
+
+// step 4
+.then(()=>{
+  return order(1000, ()=>console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`))
+})
+
+// step 5
+.then(()=>{
+  return order(1000, ()=>console.log("start the machine"))
+})
+
+// step 6
+.then(()=>{
+  return order(2000, ()=>console.log(`ice cream placed on ${stocks.holder[1]}`))
+})
+
+// step 7
+.then(()=>{
+  return order(3000, ()=>console.log(`${stocks.toppings[0]} as toppings`))
+})
+
+// Step 8
+.then(()=>{
+  return order(2000, ()=>console.log("Serve Ice Cream"))
+});
+
+// The .then handler returns a Promise when our original promise is resolved
+// It's similar to giving instructions to someone. 
+// You tell someone to " First do this, then do that, then this other thing, then.., then.., then..."
+// and so on.
+// The first task is our original promise.
+// The rest of the tasks return our promise once one small bit of work is completed
+
